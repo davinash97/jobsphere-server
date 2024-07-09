264d1c64088f5855dae83255ae5cf3e2a880ce18
davinash97 <avinashd2904@gmail.com>
2024-07-09 12:45:18 +0530

    Small Update - Adding banner - Adding Version for Maven - Common endpoints not limited to get

--- src/main/java/com/portal/jobsphere/controller/HomeController.java
+++ src/main/java/com/portal/jobsphere/controller/HomeController.java
@@ -1,29 +1,29 @@
 package com.portal.jobsphere.controller;
 
 import com.portal.jobsphere.utils.Constants;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HomeController implements Constants {
 
-	@GetMapping("/")
+	@RequestMapping("/")
 	public RedirectView redirectView() {
 		return new RedirectView(DEFAULT_URI);
 	}
 
-	@GetMapping(DEFAULT_URI)
+	@RequestMapping(DEFAULT_URI)
 	public String version() {
 		return VERSION;
 	}
 
-	@GetMapping(DEFAULT_EMPLOYEE_URI)
+	@RequestMapping(DEFAULT_EMPLOYEE_URI)
 	public RedirectView redirectEmployeeView() {
 		return new RedirectView(DEFAULT_URI);
 	}
 
-	@GetMapping(DEFAULT_EMPLOYER_URI)
+	@RequestMapping(DEFAULT_EMPLOYER_URI)
 	public RedirectView redirectEmployerView() {
 		return new RedirectView(DEFAULT_URI);
 	}
